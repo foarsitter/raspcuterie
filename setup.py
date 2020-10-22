8e3499de3bd1fc1720aaf05efc600d6c12e2ff48
Jelmer Draaijer <info@jelmert.nl>
2020-10-22 09:41:35 +0200

    setup.py for testing commands --editable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raspcuterie",
+    version="0.1",
+    py_modules=["raspcuterie"],
+    packages=find_packages(),
+    install_requires=["Click",],
+    entry_points="""
+        [console_scripts]
+        raspcuterie-cli=raspcuterie.cli:cli
+    """,
+)
