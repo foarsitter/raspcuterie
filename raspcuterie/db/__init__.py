adad9b00a045907983d4ce2d101d041290085f0e
Jelmer Draaijer <info@jelmert.nl>
2020-10-22 09:41:45 +0200

    added schema.sql

--- raspcuterie/db/__init__.py
+++ raspcuterie/db/__init__.py
@@ -8,46 +8,16 @@ db_path = base_path / "raspcuterie.db"
 connection = sqlite3.connect(db_path, check_same_thread=False)
 
 
-def init_humidity_table():
-    with connection:
-        connection.execute(
-            "create table humidity (id integer primary key, time text not null, value real not null)"
-        )
+def init_db():
+    schema_file = base_path / "raspcuterie" / "db" / "schema.sql"
 
+    with schema_file.open() as f:
+        connection.cursor().executescript(f.read())
 
-def init_temperature_table():
-    with connection:
-        connection.execute(
-            "create table temperature (id integer primary key, time text not null, value real not null)"
-        )
-
-
-def init_relay_table():
-    with connection:
-        connection.execute(
-            """create table relay
-(
-    id      int
-        constraint relay_pk
-            primary key,
-    time    text not null,
-    value_1 INTEGER default 0,
-    value_2 INTEGER default 0,
-    value_3 INTEGER default 0,
-    value_4 INTEGER default 0
-);"""
-        )
-
-
-def init_weight_table():
-    with connection:
-        connection.execute(
-            "create table weight (id integer primary key, time text not null, value real not null)"
-        )
+    print("Database Initialized")
 
 
 def insert_temperature(value: float):
-
     with connection:
         connection.execute(
             "INSERT INTO temperature(time,value) VALUES (?,?)", (datetime.now(), value)
@@ -55,7 +25,6 @@ def insert_temperature(value: float):
 
 
 def insert_humidity(value: float):
-
     with connection:
         connection.execute(
             "INSERT INTO humidity(time,value) VALUES (?,?)", (datetime.now(), value)
@@ -71,7 +40,6 @@ def insert_relay(value_1, value_2, value_3, value_4):
 
 
 def insert_weight(value: float):
-
     with connection:
         connection.execute(
             "INSERT INTO weight(time,value) VALUES (?,?)", (datetime.now(), value)
