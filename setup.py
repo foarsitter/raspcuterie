c97de7d638a303dc02143d1567c0de5911bda355
Jelmer Draaijer <info@jelmert.nl>
2020-10-30 08:29:23 +0100

    chore: lint error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     py_modules=["raspcuterie"],
     packages=find_packages(),
-    install_requires=["Click",],
+    install_requires=["Click"],
     entry_points="""
         [console_scripts]
         raspcuterie-cli=raspcuterie.cli:cli
