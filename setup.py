bb24770459bc33a782013e43232ebcd06e8a47af
jelmert <info@jelmert.nl>
2020-11-10 21:56:47 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ minimal_requirements = [
     "Click==7.1.2",
     "pyyaml==5.3.1",
     "flask-babel==2.0.0",
-    "rpi.gpio==0.7.0",
-    "Adafruit_DHT==1.4.0",
 ]
 
 
@@ -17,17 +15,22 @@ def get_long_description():
     return open("README.md", "r", encoding="utf8").read()
 
 
+rpi_requirements = ["rpi.gpio==0.7.0", "Adafruit_DHT==1.4.0"]
+
 setup(
     description="Charcuterie dashboard and controller for the Raspberry PI",
     license="MIT",
     name="raspcuterie",
-    version="0.1.3",
+    version="0.1.4",
     py_modules=["raspcuterie"],
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     url="https://github.com/foarsitter/raspcuterie",
     include_package_data=True,
-    entry_points={"console_scripts": ["raspcuterie=raspcuterie.cli:cli"]},
+    entry_points={
+        "console_scripts": ["raspcuterie=raspcuterie.cli:cli"],
+    },
     install_requires=minimal_requirements,
+    extras_require={"rpi": rpi_requirements},
 )
