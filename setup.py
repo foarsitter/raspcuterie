838a13d25ef809e84f05dce0a0e9ab1f8b718289
jelmert <info@jelmert.nl>
2020-11-11 20:18:58 +0100

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description="Charcuterie dashboard and controller for the Raspberry PI",
     license="MIT",
     name="raspcuterie",
-    version="0.1.6",
+    version="0.1.7",
     py_modules=["raspcuterie"],
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
