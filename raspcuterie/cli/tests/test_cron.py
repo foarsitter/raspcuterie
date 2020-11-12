1c5851bd851d0f6a36059864070836bbf05b7ecf
Jelmer Draaijer <info@jelmert.nl>
2020-11-12 16:18:04 +0100

    fix: rename

--- raspcuterie/cli/tests/test_cron.py
+++ raspcuterie/cli/tests/test_cron.py
@@ -2,5 +2,5 @@ from .. import cron
 
 
 def test_log_values(runner):
-    result = runner.invoke(cron.log_values, catch_exceptions=False)
+    result = runner.invoke(cron.log, catch_exceptions=False)
     assert result.exit_code == 0, result.exception
