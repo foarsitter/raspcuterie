822b811703d490e5fa53654aab10ac0596b89712
jelmert <info@jelmert.nl>
2020-12-06 15:28:26 +0100

    Bump version: 0.1.23 → 0.1.24

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Jelmer Draaijer"
 author = "Jelmer Draaijer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.23"
+release = "0.1.24"
 
 
 # -- General configuration ---------------------------------------------------
